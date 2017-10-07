ca2f3ceb2657f725f5ce2cb9b659bc0009635c6e
兔子先生 <a@tuzi.moe>
2017-10-07 19:14:39 +1300

    MySQL

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/login', '/Auth/LoginController@index')->name('login');
-Route::get('/register', '/Auth/RegisterController@index')->name('register');
