8807411e233ee9d063eda80272e205eb846dcad9
兔子先生 <a@tuzi.moe>
2017-10-07 14:30:32 +1300

    Fix Auth

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/login', '/Auth/LoginController@index')->name('login');
+Route::get('/register', '/Auth/RegisterController@index')->name('register');
